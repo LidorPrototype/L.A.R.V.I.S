648a792602b177070a1dd25ce0d908cd71815f8f
Lidor E S <ivanlidor98@gmail.com>
2019-12-30 22:16:53 +0200

    fixing conventions

--- LARVIS.py
+++ LARVIS.py
@@ -143,19 +143,22 @@ def give_date():
     now = datetime.datetime.now()
     my_date = datetime.datetime.today()
     weekday = calendar.day_name[my_date.weekday()]  # e.g. Monday
-    monthNum = now.month
-    dayNum = now.day
+    month_num = now.month
+    day_num = now.day
     month_names = ['January', 'February', 'March', 'April', 'May',
                    'June', 'July', 'August', 'September', 'October', 'November',
                    'December']
-    ordinalNumbers = ['1st', '2nd', '3rd', '4th', '5th', '6th',
-                      '7th', '8th', '9th', '10th', '11th', '12th',
-                      '13th', '14th', '15th', '16th', '17th',
-                      '18th', '19th', '20th', '21st', '22nd',
-                      '23rd', '24th', '25th', '26th', '27th',
-                      '28th', '29th', '30th', '31st']
-    larvis_response('Today is ' + weekday + ' ' + month_names[monthNum - 1] + ' the ' + ordinalNumbers[dayNum - 1] + '.')
-
+    ordinal_numbers = ['1st', '2nd', '3rd', '4th', '5th', '6th',
+                       '7th', '8th', '9th', '10th', '11th', '12th',
+                       '13th', '14th', '15th', '16th', '17th',
+                       '18th', '19th', '20th', '21st', '22nd',
+                       '23rd', '24th', '25th', '26th', '27th',
+                       '28th', '29th', '30th', '31st']
+    larvis_response('Today is '
+                    + weekday 
+                    + ' ' + month_names[month_num - 1] 
+                    + ' the ' + ordinal_numbers[day_num - 1] 
+                    + '.')
 
 def note(text):
     date = datetime.datetime.now()
