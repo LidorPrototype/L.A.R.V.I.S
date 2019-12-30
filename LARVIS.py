bbde9d5a8c75d03caaa8368a6613297aea2b1510
Lidor E S <ivanlidor98@gmail.com>
2019-12-30 22:12:56 +0200

    updating import and adding ability
    adding calendar import and adding ability to say today's date

--- LARVIS.py
+++ LARVIS.py
@@ -19,6 +19,7 @@ from time import strftime
 import wolframalpha
 import pytz
 import requests
+import calendar
 
 """
     Create By Lidor Eliyahu S.
@@ -136,6 +137,24 @@ def get_date(text):
         return today + datetime.timedelta(dif)
     if day != -1:
         return datetime.date(month=month, day=day, year=year)
+
+
+def giveDate():
+    now = datetime.datetime.now()
+    my_date = datetime.datetime.today()
+    weekday = calendar.day_name[my_date.weekday()]  # e.g. Monday
+    monthNum = now.month
+    dayNum = now.day
+    month_names = ['January', 'February', 'March', 'April', 'May',
+                   'June', 'July', 'August', 'September', 'October', 'November',
+                   'December']
+    ordinalNumbers = ['1st', '2nd', '3rd', '4th', '5th', '6th',
+                      '7th', '8th', '9th', '10th', '11th', '12th',
+                      '13th', '14th', '15th', '16th', '17th',
+                      '18th', '19th', '20th', '21st', '22nd',
+                      '23rd', '24th', '25th', '26th', '27th',
+                      '28th', '29th', '30th', '31st']
+    larvis_response('Today is ' + weekday + ' ' + month_names[monthNum - 1] + ' the ' + ordinalNumbers[dayNum - 1] + '.')
 
 
 def note(text):
