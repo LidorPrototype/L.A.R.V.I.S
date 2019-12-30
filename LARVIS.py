2d9048faf5c89e5afa977f5c95806ff768aa7329
Lidor E S <ivanlidor98@gmail.com>
2019-12-30 21:40:18 +0200

    adding global variables

--- LARVIS.py
+++ LARVIS.py
@@ -25,3 +25,11 @@ import requests
     All rights reserved
     Please see README.txt for instructions
 """
+
+# SCOPES = []   Get this from https://developers.google.com/calendar/quickstart/python
+MONTHS = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november",
+          "december"]
+DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
+CALENDAR_OPTIONS = ["what do i have", "do i have plans", "am i busy"]
+NOTE_OPTIONS = ["make a note", "write this down", "remember this"]
